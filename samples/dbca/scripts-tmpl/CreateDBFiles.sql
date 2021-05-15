af6cff757202c4451a1bfa8082f56f8c30d0557b
Hajime Ohnishi <hajime.ohnishi@gmail.com>
2021-05-15 23:40:40 +0900

    fix

--- samples/dbca/scripts-tmpl/CreateDBFiles.sql
+++ samples/dbca/scripts-tmpl/CreateDBFiles.sql
@@ -2,8 +2,6 @@ SET VERIFY OFF
 connect "SYS"/"&&sysPassword" as SYSDBA
 set echo on
 spool {{.LOG_DIR}}\CreateDBFiles.log append
-{{range .tbss}}
-{{template "create_tablespace" . -}}
-{{end}}
+{{range .tbss}}{{template "create_tablespace" .}}{{end}}
 ALTER DATABASE DEFAULT TABLESPACE "USERS";
 spool off
