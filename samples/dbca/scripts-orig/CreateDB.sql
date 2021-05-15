0e05820b203223a6af74eb7864638db3997f35c9
Hajime Ohnishi <hajime.ohnishi@gmail.com>
2021-05-15 22:31:36 +0900

    fix

--- samples/dbca/scripts-orig/CreateDB.sql
+++ samples/dbca/scripts-orig/CreateDB.sql
@@ -1,62 +1,40 @@
+SET VERIFY OFF
+connect "SYS"/"&&sysPassword" as SYSDBA
+set echo on
+spool C:\app\Administrator\admin\orcl\scripts\CreateDB.log append
+startup nomount pfile="C:\app\Administrator\admin\orcl\scripts\init.ora";
 CREATE DATABASE "orcl"
-  USER SYS IDENTIFIED BY "manager"
-  USER SYSTEM IDENTIFIED BY "manager"
-  CONTROLFILE REUSE
+  USER SYS IDENTIFIED BY "&&sysPassword"
+  USER SYSTEM IDENTIFIED BY "&&systemPassword"
   MAXDATAFILES 1024
   MAXINSTANCES 8
   CHARACTER SET AL32UTF8
   NATIONAL CHARACTER SET AL16UTF16
-  SET DEFAULT SMALLFILE TABLESPACE
-
   LOGFILE
-    GROUP 1 ( 'C:\app\Administrator\oradata\ORCL\redo01.log', 'C:\app\Administrator\oradata\ORCL\redo02.log', 'C:\app\Administrator\oradata\ORCL\redo03.log' ) SIZE 200M BLOCKSIZE 16K REUSE,
-    GROUP 2 ( 'C:\app\Administrator\oradata\ORCL\redo01.log', 'C:\app\Administrator\oradata\ORCL\redo02.log', 'C:\app\Administrator\oradata\ORCL\redo03.log' ) SIZE 200M BLOCKSIZE 16K REUSE,
-    GROUP 3 ( 'C:\app\Administrator\oradata\ORCL\redo01.log', 'C:\app\Administrator\oradata\ORCL\redo02.log', 'C:\app\Administrator\oradata\ORCL\redo03.log' ) SIZE 200M BLOCKSIZE 16K REUSE
+    GROUP 1 ( 'C:\app\Administrator\oradata\ORCL\redo01.log' ) SIZE 200M,
+    GROUP 2 ( 'C:\app\Administrator\oradata\ORCL\redo02.log' ) SIZE 200M,
+    GROUP 3 ( 'C:\app\Administrator\oradata\ORCL\redo03.log' ) SIZE 200M
   MAXLOGFILES 16
   MAXLOGMEMBERS 3
   MAXLOGHISTORY 1
-  ARCHIVELOG
-  FORCE LOGGING
-  SET STANDBY NOLOGGING FOR DATA AVAILABILITY
-
   EXTENT MANAGEMENT LOCAL
   DATAFILE
-     'C:\app\Administrator\oradata\ORCL\system01.dbf' SIZE 700M REUSE AUTOEXTEND ON NEXT 10240K MAXSIZE UNLIMITED,
-     'C:\app\Administrator\oradata\ORCL\system02.dbf' SIZE 700M REUSE AUTOEXTEND ON NEXT 10240K MAXSIZE UNLIMITED
+     'C:\app\Administrator\oradata\ORCL\system01.dbf' SIZE 700M REUSE AUTOEXTEND ON NEXT 10240K MAXSIZE UNLIMITED
   SYSAUX DATAFILE
-     'C:\app\Administrator\oradata\ORCL\sysaux01.dbf' SIZE 550M REUSE AUTOEXTEND ON NEXT 10240K MAXSIZE UNLIMITED,
-     'C:\app\Administrator\oradata\ORCL\sysaux02.dbf' SIZE 550M REUSE AUTOEXTEND ON NEXT 10240K MAXSIZE UNLIMITED
-  DEFAULT TABLESPACE USERS
-    DATAFILE
-      'C:\app\Administrator\oradata\ORCL\users01.dbf' SIZE 5M REUSE AUTOEXTEND ON NEXT 1280K MAXSIZE UNLIMITED
-    EXTENT MANAGEMENT LOCAL
+     'C:\app\Administrator\oradata\ORCL\sysaux01.dbf' SIZE 550M REUSE AUTOEXTEND ON NEXT 10240K MAXSIZE UNLIMITED
   SMALLFILE DEFAULT TEMPORARY TABLESPACE TEMP
     TEMPFILE
-      'C:\app\Administrator\oradata\ORCL\temp01.dbf' SIZE 20M REUSE AUTOEXTEND ON NEXT 640K MAXSIZE UNLIMITED,
-      'C:\app\Administrator\oradata\ORCL\temp02.dbf' SIZE 20M REUSE AUTOEXTEND ON NEXT 640K MAXSIZE UNLIMITED
-    EXTENT MANAGEMENT LOCAL
-  SMALLFILE UNDO TABLESPACE UNDOTBS1
+      'C:\app\Administrator\oradata\ORCL\temp01.dbf' SIZE 20M REUSE AUTOEXTEND ON NEXT 640K MAXSIZE UNLIMITED
+  SMALLFILE UNDO TABLESPACE "UNDOTBS1"
     DATAFILE
-      'C:\app\Administrator\oradata\ORCL\undotbs01.dbf' SIZE 200M REUSE AUTOEXTEND ON NEXT 5120K MAXSIZE UNLIMITED,
-      'C:\app\Administrator\oradata\ORCL\undotbs02.dbf' SIZE 200M REUSE AUTOEXTEND ON NEXT 5120K MAXSIZE UNLIMITED
-
-  SET TIME_ZONE = 'Asia/Tokyo'
-
-  SMALLFILE USER_DATA TABLESPACE USERDATA
-    DATAFILE
-     'C:\app\Administrator\oradata\ORCL\user01.dbf' SIZE 200M REUSE AUTOEXTEND ON NEXT 5120K MAXSIZE UNLIMITED,
-     'C:\app\Administrator\oradata\ORCL\user02.dbf' SIZE 200M REUSE AUTOEXTEND ON NEXT 5120K MAXSIZE UNLIMITED
-
+      'C:\app\Administrator\oradata\ORCL\undotbs01.dbf' SIZE 200M REUSE AUTOEXTEND ON NEXT 5120K MAXSIZE UNLIMITED
   ENABLE PLUGGABLE DATABASE
   SEED
     FILE_NAME_CONVERT = (
-      '/u01/app/oracle/oradata/cdb/system01.dbf', '/u01/app/oracle/oradata/pdbseed/system01.dbf', 
-      '/u01/app/oracle/oradata/cdb/', '/u01/app/oracle/oradata/pdbseed/'
+      'C:\app\Administrator\oradata\ORCL\system01.dbf', 'C:\app\Administrator\oradata\ORCL\pdbseed\system01.dbf',
+      'C:\app\Administrator\oradata\ORCL\sysaux01.dbf', 'C:\app\Administrator\oradata\ORCL\pdbseed\sysaux01.dbf',
+      'C:\app\Administrator\oradata\ORCL\temp01.dbf', 'C:\app\Administrator\oradata\ORCL\pdbseed\temp01.dbf',
+      'C:\app\Administrator\oradata\ORCL\undotbs01.dbf', 'C:\app\Administrator\oradata\ORCL\pdbseed\undotbs01.dbf'
     )
-    SYSTEM DATAFILES SIZE 5M AUTOEXTEND ON NEXT 1280K MAXSIZE UNLIMITED
-    SYSAUX DATAFILES SIZE 5M AUTOEXTEND ON NEXT 1280K MAXSIZE UNLIMITED
-  LOCAL UNDO ON
 ;
-
-
-
+spool off
--- samples/dbca/scripts-tmpl/CreateDB.sql
+++ samples/dbca/scripts-tmpl/CreateDB.sql
@@ -1,46 +1,14 @@
-{{/* optional param */ -}}
+{{/*
 {{$maxinstances  := or .maxinstances  8 -}}
 {{$maxloghistory := or .maxloghistory 1 -}}
 {{$maxlogfiles   := or .maxlogfiles   16 -}}
 {{$maxlogmembers := or .maxlogmembers 3 -}}
 {{$maxdatafiles  := or .maxdatafiles  1024 -}}
-{{/* ================ */ -}}
-{{template "create_database" .}}
-
-{{/*
+*/}}
 SET VERIFY OFF
 connect "SYS"/"&&sysPassword" as SYSDBA
 set echo on
 spool {{.LOG_DIR}}\CreateDB.log append
 startup nomount pfile="{{.SCRIPT_DIR}}\init.ora";
-CREATE DATABASE "{{.SID}}"
-MAXINSTANCES {{$maxinstances}}
-MAXLOGHISTORY {{$maxloghistory}}
-MAXLOGFILES {{$maxlogfiles}}
-MAXLOGMEMBERS {{$maxlogmembers}}
-MAXDATAFILES {{$maxdatafiles}}
-DATAFILE {{- template "datafile_tempfile_spec" .SYSTEM }}
-EXTENT MANAGEMENT LOCAL
-SYSAUX
-  DATAFILE {{- template "datafile_tempfile_spec" .SYSAUX }}
-SMALLFILE DEFAULT TEMPORARY TABLESPACE {{.TEMP.tbsname}}
-  TEMPFILE {{- template "datafile_tempfile_spec" .TEMP }}
-SMALLFILE UNDO TABLESPACE "{{.UNDO.tbsname}}"
-  DATAFILE {{- template "datafile_tempfile_spec" .UNDO }}
-CHARACTER SET {{.charset}}
-NATIONAL CHARACTER SET {{.nscharset}}
-LOGFILE
-  GROUP 1 ('{{.DB_BASE}}\redo01.log') SIZE 200M,
-  GROUP 2 ('{{.DB_BASE}}\redo02.log') SIZE 200M,
-  GROUP 3 ('{{.DB_BASE}}\redo03.log') SIZE 200M
-USER SYS IDENTIFIED BY "&&sysPassword"
-USER SYSTEM IDENTIFIED BY "&&systemPassword"
-enable pluggable database
-seed file_name_convert=(
-  '{{.SYSTEM.path}}','{{.SEED_BASE}}\{{.SYSTEM.fname}}',
-  '{{.SYSAUX.path}}','{{.SEED_BASE}}\{{.SYSAUX.fname}}',
-  '{{.TEMP.path}}','{{.SEED_BASE}}\{{.TEMP.fname}}',
-  '{{.UNDO.path}}','{{.SEED_BASE}}\{{.UNDO.fname}}'
-  ) LOCAL UNDO ON;
+{{template "create_database" .}}
 spool off
-*/}}
